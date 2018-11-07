09d1cdb180ac90182c1797c984fab97cd02d509d
Coombszy <LCCoombs@hotmail.co.uk>
2018-11-07 11:05:10 +0000

    completed

--- src/interface.js
+++ src/interface.js
@@ -32,9 +32,18 @@ $(document).ready(function() {
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
+    displayWeather();
     updatePSM();
   }
   function updatePSM(){
     $('#psm-status').text(thermostat.psmState());
+  }
+  function displayWeather() {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=Reading';
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    })
   }
 });
